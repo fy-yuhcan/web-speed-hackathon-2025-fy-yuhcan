ed58615ea089e70c74147da81c475e83a05b1c4c
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2026-02-15 01:42:50 +0900

    perf(polyfills): load legacy polyfills only when required

--- workspaces/client/src/setups/polyfills.ts
+++ workspaces/client/src/setups/polyfills.ts
@@ -1,5 +1,15 @@
-import 'core-js';
-import 'setimmediate';
+const hasWindow = typeof window !== 'undefined';
+
+if (
+  hasWindow &&
+  (!('Promise' in window) || !('fetch' in window) || !('URL' in window) || !('queueMicrotask' in window))
+) {
+  void import('core-js');
+}
+
+if (typeof globalThis.setImmediate !== 'function') {
+  void import('setimmediate');
+}
 
 if (!('startViewTransition' in document)) {
   void import('view-transitions-polyfill');
